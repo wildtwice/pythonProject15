2e86e30b1c64f4af56fe668a3b73167248be8b3e
pjb75 <pjb7525@gmail.com>
2022-02-19 16:00:28 +0900

    Initial commit

---
+++ 변수 바꾸기.py
@@ -0,0 +1,30 @@
+#week 6
+
+'''x = 10
+y = 20
+z = 0
+z = x
+x = y
+y = z
+print(x, y)
+
+mamber = int(input("참석자 수를 입력하세요>>"))
+print(mamber)
+print(mamber*2)
+print(mamber*4)
+'''
+
+'''sum = 0
+for x in range(11):
+    sum = sum + x
+print(sum)
+'''
+
+'''a = int(input("시작의 숫자를 입력하세요."))
+b = int(input("끝나는 숫자를 입력하세요."))
+sum = 0
+for x in range(a,b+1):
+    if x%2!=0:
+        sum = sum + x
+print(sum)
+'''
